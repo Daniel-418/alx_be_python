85db8776e8efb85d755b26c9f19269ca9a8cb9eb
Daniel Komolafe <komolafedanielolaoluwa@gmail.com>
2025-06-10 19:15:47 -0700

    .

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,16 +12,16 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
         elif choice == '2':
-            item = input("Enter item to remove: ")
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
             else:
                 print("{} not in shopping list".format(item))
         elif choice == '3':
-            counter = 0
+            counter = 1
             for item in shopping_list:
                 print("{}. {}".format(counter, item))
                 counter += 1
