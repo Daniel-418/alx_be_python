1c228972b3c33d42c02f8fbf87f2358b4ad10586
Komolafe Daniel <komolafedanielolaoluwa@gmail.com>
2025-06-28 19:12:24 -0700

    .

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,15 +1,15 @@
 class Book:
     def __init__(self, title, author, year):
-        self.__title = title
-        self.__author = author
-        self.__year = year
+        self.title = title
+        self.author = author
+        self.year = year
 
     def __del__(self):
-        print(f"Deleting {self.__title}")
+        print(f"Deleting {self.title}")
 
     def __str__(self):
-        return (f"{self.__title} by {self.__author}, published " +
-                f"in {self.__year}")
+        return (f"{self.title} by {self.author}, published " +
+                f"in {self.year}")
 
     def __repr__(self):
-         return (f"Book('{self.__title}', '{self.__author}', '{self.__year}')")
+         return (f"Book('{self.title}', '{self.author}', '{self.year}')"
